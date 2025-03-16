e330e6dc7e623696cd18d0c210c8e41c200b7016
[Mugunthan Muthuraman] <Mugunthan>
2025-03-16 18:30:43 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -203,25 +203,3 @@ function openPopup() {
 function closePopup() {
     popup.classList.remove('open-popup');
 }
-//// /////////////////////////////////////////////////////////////////////
-// const search = () => {
-//     const searchbox = document.getElementById("search-box").value.toUpperCase();
-//     const storeitems = document.getElementById('cart-items-container')
-//     const product = document.querySelectorAll('.cart-item')
-//     const pname = document.getElementsByTagName("h3")
-
-//     for (var i = 0; i < pname.length; i++) {
-//         let match = product[i].getElementsByTagName('h3')[0];
-
-//         if (match) {
-//             let textvalue = match.textContent || match.innerHTML
-
-//             if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
-//                 product[i].style.display = "";
-//             }
-//             else {
-//                 product[i].style.display = "none";
-//             }
-//         }
-//     }
-// }
